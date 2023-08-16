ce0d2c876a47f4c2b154fbfce4d28f0f1b2b66e6
krieger <claudio_krieger@hotmail.com>
2023-08-16 18:43:26 -0400

    se agregan a ignore, se modifica env, signIn y signUp funcionando

--- models/index.js
+++ models/index.js
@@ -10,6 +10,26 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.actor = require("./skater.model")(sequelize, Sequelize);
+db.skater = require("./skater.model")(sequelize, Sequelize);
+
+(async ()=>{
+  try {
+    await sequelize.sync({force:true});
+    await  db.skater.create({// siempre quedara con id 1
+      email:'admin@js.cl',
+      nombre:'admin',
+      password:'$2a$10$g9HOu3HC53QYaih1lzqsmuYn3dvcQp5O./brK/AKV9UxeiCG79.le',//admin
+      anosExperiencia:99,
+      especialidad:'ADMIN',
+      foto:'',
+      estado:true
+    });
+    console.log("Tablas creadas! con force")
+  } catch (error) {
+    console.error("Error al crear tablas");
+  } 
+
+
+})();
 
 module.exports = db;
